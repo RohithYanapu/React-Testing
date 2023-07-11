850eb7076149864a98928565bb93787723885362
RohithYanapu <rohith.yanapu@gmail.com>
2023-07-11 17:59:59 -0500

    Added Auth0

--- src/Components/Appbar.js
+++ src/Components/Appbar.js
@@ -5,6 +5,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import LoginButton from "./LoginButton"; 
+import LogoutButton from "./LogoutButton"; 
 
 export default function Appbar() {
   return (
@@ -23,6 +25,8 @@ export default function Appbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Student System
           </Typography>
+          <LoginButton/>
+          <LogoutButton/>
         </Toolbar>
       </AppBar>
     </Box>
